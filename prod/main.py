b48e46bf210a0622ad495a5ef8c2a511628f2a69
paBuild1local <sean.langre@pocappstudios.com>
2022-12-23 13:43:32 +0100

    Updated alot // oskar

--- prod/main.py
+++ prod/main.py
@@ -3,6 +3,7 @@ import os
 from threading import ThreadError
 import discord
 import enum
+import psutil
 # import asyncio
 import warnings
 warnings.filterwarnings("ignore")
@@ -18,6 +19,7 @@ class ExtendedEnum(enum.Enum):
 
 class discCommands(ExtendedEnum):
     BUILD = "$build"
+    SAVE_SNAPSHOT = "$snapshot"
     HELP = "help"
 
 class Remotes(ExtendedEnum):
@@ -46,18 +48,35 @@ async def on_ready():
 async def on_message(message):
     if message.author == client.user:
         return
+
     if message.content.startswith(discCommands.BUILD.value):
-        try:
-            await build(message, message.content)
-        except info as e:
-            print("info! {}".format(e))
-        except warning as e:
-            print("Warning! {}".format(e))
-        except Exception as e:
-            print("Exception! {}".format(e))
+        if checkIfProcessRunning("Unity.exe"):
+            await message.channel.send("Unity was already running can´t complete command!")
+        else:
+            try:
+                await build(message, message.content)
+            except info as e:
+                print("info! {}".format(e))
+            except warning as e:
+                print("Warning! {}".format(e))
+            except Exception as e:
+                print("Exception! {}".format(e))
         
+    if message.content.startswith(discCommands.SAVE_SNAPSHOT.value):
+        if checkIfProcessRunning("Unity.exe"):
+            await message.channel.send("Unity was already running can´t complete command!")
+        else :
+            try:
+                await saveSnapshot(message, message.content)
+            except info as e:
+                print("info! {}".format(e))
+            except warning as e:
+                print("Warning! {}".format(e))
+            except Exception as e:
+                print("Exception! {}".format(e))
+
     if discCommands.HELP.value in message.content:
-        await message.channel.send("Example1: $build integration -mock -noabb Example2: $build integration -dev-remote -noabb Example3: $build integration -prod-remote -abb")
+        await message.channel.send("Build commands:\n   Example 1: $build integration -mock -noabb\n    Example2: $build integration -dev-remote -noabb\n   Example3: $build integration -prod-remote -abb\nSnapshot command:\n    $snapshot [PLAYERID],[PLAYERID],...")
 
 async def build(message, content):
     splitted = content.split()
@@ -66,7 +85,7 @@ async def build(message, content):
     
     await message.channel.send("Starting Build {}".format(message.author))
     if(Remotes.MOCK.value in content):
-        print("Build {}".format(Remotes.MOCK.value))
+        print("Build remote:{} git:{} isABB: {}".format(Remotes.MOCK.value, gitbranch, isAbb))
         command = [r"" + os.getenv("BUILD_BOT_PATH"), gitbranch, Remotes.MOCK.value, isAbb]
         await buildPrint(message, command)
     elif(Remotes.DEV.value in content):
@@ -83,6 +102,29 @@ async def build(message, content):
 async def buildPrint(message, command):
     result = run(command, stdout=PIPE, stderr=PIPE, universal_newlines=True)
 
+
+async def saveSnapshot(message, content):
+    splitted = content.split()
+    playerIds = f"\"{splitted[1]}\""
+    await message.channel.send("Starting Unity to take snapshots for {}".format(message.author))
+    print("Snapshot playerIds {}".format(playerIds))
+    command = [r"" + os.getenv("SNAPSHOT_BOT_PATH"), playerIds]
+    await buildPrint(message, command)
+
+def checkIfProcessRunning(processName):
+    '''
+    Check if there is any running process that contains the given name processName.
+    '''
+    #Iterate over the all the running process
+    for proc in psutil.process_iter():
+        try:
+            # Check if process name contains the given name string.
+            if processName.lower() in proc.name().lower():
+                return True
+        except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
+            pass
+    return False;
+
 # await client.loop.run_until_complete(func)
 # await client.loop.run_until_complete(func)
-client.run(os.getenv("DISCORD_TOKEN"), log_handler=None)
+client.run(os.getenv("DISCORD_TOKEN"))
